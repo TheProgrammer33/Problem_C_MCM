4e2f4e0d3d12dde3da41d33c153a4c0b427b57dd
Joshua_Burdick <31868836+Joshua-Burdick@users.noreply.github.com>
2022-02-18 17:54:51 -0500

    Expanded to operate on finalData.csv

--- priceEffect.py
+++ priceEffect.py
@@ -1,29 +1,37 @@
 import pandas as pd
 
-# Currently just for Bitcoin
+datastream = pd.read_csv("./Data/finalData.csv")
 
-bitcoinRd = pd.read_csv("./Data/BCHAIN-MKPRU.csv")
+outfile = open("./Data/finaldataPE.csv", "w")
+outfile.write("Date,USD (PM),BTC Price,Delta(G),Delta(B),PE(G),PE(B)\n")
 
-f = open("output_test.txt", "w")
-f.write("Date\t-- Value  -- Delta(B) -- PE(B)\n")
-
-prevVal = bitcoinRd["Value"][0]
-f.write(bitcoinRd["Date"][0] + " -- " + str(prevVal) + " -- null -- -1\n")
+prevValGold = datastream["USD (PM)"][0]
+prevValBTC = datastream["BTC Price"][0]
+outfile.write(datastream["Date"][0] + "," + str(prevValGold) + "," + str(prevValBTC) + ",null,-1,null,-1\n")
 
 count = 1
 
-for date in bitcoinRd["Date"][1:]:
-    f.write(date + " -- ")
-    curVal = bitcoinRd["Value"][count]
-    f.write(str(curVal) + " -- ")
-    delta = round(curVal - prevVal, 4)
+for date in datastream["Date"][1:]:
+    outfile.write(date + ",")
+    curValGold = datastream["USD (PM)"][count]
+    curValBTC = datastream["BTC Price"][count]
+    outfile.write(str(curValGold) + "," + str(curValBTC) + ",")
+    deltaGold = round(curValGold - prevValGold, 4)
+    deltaBTC = round(curValBTC - prevValBTC, 4)
     
-    f.write(str(delta) + " -- ")
+    outfile.write(str(deltaGold) + "," + str(deltaBTC) + ",")
 
-    if (delta > 0):
-        f.write("1")
-    elif (delta < 0):
-        f.write("0")
-    prevVal = curVal
+    if (deltaGold > 0):
+        outfile.write("1,")
+    elif (deltaGold < 0):
+        outfile.write("0,")
+    prevValGold = curValGold
+
+    if (deltaBTC > 0):
+        outfile.write("1")
+    elif (deltaBTC < 0):
+        outfile.write("0")
+    prevValBTC = curValBTC
+
     count+=1
-    f.write("\n")
+    outfile.write("\n")
