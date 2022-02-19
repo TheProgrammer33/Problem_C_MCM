09f35533009d289281604c4b56c24e506419522f
TheProgrammer33 <dylan.m.bryant@gmail.com>
2022-02-19 17:08:34 -0500

    Added Possible Start Days
    Updated to test possible start days.

--- main.py
+++ main.py
@@ -17,35 +17,33 @@ def main():
     btcGoldDF = pd.read_csv('./Data/finalData.csv')
     results = {"Training Days": [], "Prediction": [], "Actual": [], "Accurate": []}
 
-    for trainingDays in range(10, len(btcGoldDF)-11):
-        lastTrainingDayPrice = btcGoldDF["Gold Price"][trainingDays]
-        predictionForNextDay = getRiseFall(lastTrainingDayPrice, regressionModels.kNeighbors(trainingDays)[0])
-        actualNextDay = getRiseFall(lastTrainingDayPrice, btcGoldDF["Gold Price"][trainingDays+1])
+    # Change range values (start day, lastStartDay)
+    for startDay in range(10, 1500):
+        for trainingDays in range(startDay, len(btcGoldDF)-1):
+            lastTrainingDayPrice = btcGoldDF["Gold Price"][trainingDays]
+            predictionForNextDay = getRiseFall(lastTrainingDayPrice, regressionModels.MLPNN(trainingDays)[0])
+            actualNextDay = getRiseFall(lastTrainingDayPrice, btcGoldDF["Gold Price"][trainingDays+1])
 
-        results["Training Days"].append(lastTrainingDayPrice)
-        results["Prediction"].append(predictionForNextDay)
-        results["Actual"].append(actualNextDay)
-        results["Accurate"].append(1 if predictionForNextDay == actualNextDay else 0)
+            results["Training Days"].append(lastTrainingDayPrice)
+            results["Prediction"].append(predictionForNextDay)
+            results["Actual"].append(actualNextDay)
+            results["Accurate"].append(1 if predictionForNextDay == actualNextDay else 0)
 
-        outputPredictions.append([predictionForNextDay, actualNextDay])
+        resultDF = pd.DataFrame(results)
+        resultDF.to_csv('./NeuralNetworkResults/trainingResults' + str(startDay) +'.csv', index=False, columns=["Training Days", "Prediction", "Actual", "Accurate"])
 
-    resultDF = pd.DataFrame(results)
-    resultDF.to_csv('./Data/trainingResults.csv', index=False, columns=["Training Days", "Prediction", "Actual", "Accurate"])
+        summedPredictions = 0
+        for accurate in results["Accurate"]:
+            summedPredictions += accurate * 100
+        
+        accuracy = summedPredictions / len(results["Accurate"])
+        outfile = open('./NeuralNetworkResults/trainingResults' + str(startDay) + '.csv', "a")
+        outfile.write(str(accuracy))
+        outfile.close()
+
 
     t1 = time.time()
     print("Time Required: " + str(t1-t0))
-
-    summedPredictions = 0
-
-    for output in outputPredictions:
-        if output[0] == output[1]:
-            summedPredictions += 100
-        else:
-            summedPredictions += 0
-    
-    accuracy = summedPredictions / len(outputPredictions)
-
-    print("Final accuracy: " + str(accuracy) + "%")
 
     #dataCleaner.fillGold()
 
