9f89b4c9fcb269e1909094cd1398d13d78c0dd8c
Raidienaty <jcanducci@gmail.com>
2022-02-19 19:28:22 -0500

    Built multiprocessing
    Added multiprocessing for filing through the models

--- main.py
+++ main.py
@@ -1,10 +1,60 @@
 import pandas as pd
 import time
+from multiprocessing import Process
 
 import dataAnalyzer, dataCleaner
 import regressionModels
 import classificationModels
 import priceEffect
+
+NUM_PROC = 4
+
+btcGoldDF = pd.read_csv('./Data/finalData.csv')
+
+def predictDays(bs, startDay):
+    results = {"Training Days": [], "Prediction": [], "Actual": [], "Accurate": []}
+
+    for trainingDays in range(startDay, len(btcGoldDF)-1):
+        lastTrainingDayPrice = btcGoldDF["Gold Price"][trainingDays]
+        predictionForNextDay = getRiseFall(lastTrainingDayPrice, regressionModels.RandomForest(trainingDays)[0])
+        actualNextDay = getRiseFall(lastTrainingDayPrice, btcGoldDF["Gold Price"][trainingDays+1])
+
+        results["Training Days"].append(lastTrainingDayPrice)
+        results["Prediction"].append(predictionForNextDay)
+        results["Actual"].append(actualNextDay)
+        results["Accurate"].append(1 if predictionForNextDay == actualNextDay else 0)
+
+    resultDF = pd.DataFrame(results)
+    resultDF.to_csv('./MLPredictionResults/trainingResults' + str(startDay) +'.csv', index=False, columns=["Training Days", "Prediction", "Actual", "Accurate"])
+
+    summedPredictions = 0
+    for accurate in results["Accurate"]:
+        summedPredictions += accurate * 100
+    
+    accuracy = summedPredictions / len(results["Accurate"])
+    outfile = open('./MLPredictionResults/trainingResults' + str(startDay) + '.csv', "a")
+    outfile.write(str(accuracy))
+    outfile.close()
+
+
+def train():
+    # Change range values (start day, lastStartDay)
+    for startDay in range(11, 15, NUM_PROC):
+        jobs = []
+
+        for i in range(NUM_PROC):
+            process = Process(
+                target=predictDays, 
+                args=([], startDay+i)
+            )
+            jobs.append(process)
+
+        for j in jobs:
+            j.start()
+
+        for j in jobs:
+            j.join()
+
 
 def main():
     print("Starting...")
@@ -12,36 +62,8 @@ def main():
     dataCleaner.combineCSVs()
     priceEffect.setPriceEffectToFile()
 
-    outputPredictions = []
     t0 = time.time()
-    btcGoldDF = pd.read_csv('./Data/finalData.csv')
-    results = {"Training Days": [], "Prediction": [], "Actual": [], "Accurate": []}
-
-    # Change range values (start day, lastStartDay)
-    for startDay in range(10, 1500):
-        for trainingDays in range(startDay, len(btcGoldDF)-1):
-            lastTrainingDayPrice = btcGoldDF["Gold Price"][trainingDays]
-            predictionForNextDay = getRiseFall(lastTrainingDayPrice, regressionModels.MLPNN(trainingDays)[0])
-            actualNextDay = getRiseFall(lastTrainingDayPrice, btcGoldDF["Gold Price"][trainingDays+1])
-
-            results["Training Days"].append(lastTrainingDayPrice)
-            results["Prediction"].append(predictionForNextDay)
-            results["Actual"].append(actualNextDay)
-            results["Accurate"].append(1 if predictionForNextDay == actualNextDay else 0)
-
-        resultDF = pd.DataFrame(results)
-        resultDF.to_csv('./NeuralNetworkResults/trainingResults' + str(startDay) +'.csv', index=False, columns=["Training Days", "Prediction", "Actual", "Accurate"])
-
-        summedPredictions = 0
-        for accurate in results["Accurate"]:
-            summedPredictions += accurate * 100
-        
-        accuracy = summedPredictions / len(results["Accurate"])
-        outfile = open('./NeuralNetworkResults/trainingResults' + str(startDay) + '.csv', "a")
-        outfile.write(str(accuracy))
-        outfile.close()
-
-
+    train()
     t1 = time.time()
     print("Time Required: " + str(t1-t0))
 
